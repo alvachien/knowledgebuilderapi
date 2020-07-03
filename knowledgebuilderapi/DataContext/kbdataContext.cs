3d09ad86401021fadb577a3b6c83d278a7ea5762
Alva Chien <alvachien@163.com>
2020-07-04 00:58:03 +0800

    Add new changes.

--- knowledgebuilderapi/DataContext/kbdataContext.cs
+++ knowledgebuilderapi/DataContext/kbdataContext.cs
@@ -6,24 +6,80 @@ namespace knowledgebuilderapi.Models
     public class kbdataContext : DbContext
     {
         public kbdataContext(DbContextOptions<kbdataContext> options) : base(options)
-        { 
+        {
+            TestingMode = false;
         }
 
+        public kbdataContext(DbContextOptions<kbdataContext> options, bool testingMode = false) : base(options)
+        {
+            TestingMode = testingMode;
+        }
+
+        // Testing mode
+        public Boolean TestingMode { get; private set; }
+
         public DbSet<KnowledgeItem> KnowledgeItems { get; set; }
+        public DbSet<QuestionBankItem> QuestionBankItems { get; set; }
+        public DbSet<QuestionBankSubItem> QuestionBankSubItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<KnowledgeItem>()
-                .Property(b => b.CreatedAt);
-                // .HasDefaultValueSql("getdate()");
-            modelBuilder.Entity<KnowledgeItem>()
-                .Property(b => b.ModifiedAt);
-                // .HasDefaultValueSql("getdate()");
-            modelBuilder.Entity<KnowledgeItem>()
-                .Property(e => e.Category)
-                .HasConversion(
-                    v => (Int16)v,
-                    v => (KnowledgeCategory)v);
+            modelBuilder.Entity<KnowledgeItem>(entity =>
+            {
+                if (!TestingMode)
+                {
+                    entity.Property(b => b.CreatedAt)
+                        .HasDefaultValueSql("GETDATE()");
+                    entity.Property(b => b.ModifiedAt)
+                        .HasDefaultValueSql("GETDATE()");
+                }
+                else
+                {
+                    entity.Property(b => b.CreatedAt)
+                        .HasDefaultValueSql("CURRENT_DATE");
+                    entity.Property(b => b.ModifiedAt)
+                        .HasDefaultValueSql("CURRENT_DATE");
+                }
+                entity.Property(b => b.Category)
+                    .HasConversion(
+                        v => (Int16)v,
+                        v => (KnowledgeCategory)v);
+            });
+
+            modelBuilder.Entity<QuestionBankItem>(entity =>
+            {
+                if (!TestingMode)
+                {
+                    entity.Property(b => b.CreatedAt)
+                        .HasDefaultValueSql("GETDATE()");
+                    entity.Property(b => b.ModifiedAt)
+                        .HasDefaultValueSql("GETDATE()");
+                }
+                else
+                {
+                    entity.Property(b => b.CreatedAt)
+                        .HasDefaultValueSql("CURRENT_DATE");
+                    entity.Property(b => b.ModifiedAt)
+                        .HasDefaultValueSql("CURRENT_DATE");
+                }
+
+                entity.HasOne(d => d.CurrentKnowledgeItem)
+                    .WithMany(p => p.QuestionBankItems)
+                    .HasForeignKey(d => d.KnowledgeItemID)
+                    .OnDelete(DeleteBehavior.SetNull)
+                    .HasConstraintName("FK_QBITEM_KITEM");
+            });
+
+            modelBuilder.Entity<QuestionBankSubItem>(entity =>
+            {
+                entity.HasKey(e => new { e.ItemID, e.SubID });
+
+                entity.HasOne(d => d.CurrentQuestionBankItem)
+                    .WithMany(p => p.SubItems)
+                    .HasForeignKey(d => d.ItemID)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_QBSUBITEM_QBITEM");
+            });
         }
     }
 }
