9ca2e91d8d9a27a489a9e334442af9846c6dfd7a
Alva Chien <alvachien@live.com>
2021-07-10 23:17:34 +0800

    Add Access code mode

---
+++ knowledgebuilderapi/Controllers/AccessCodeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace knowledgebuilderapi.Controllers
+{
+    public sealed class AccessCode
+    {
+        public string InputtedCode { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccessCodeController : ControllerBase
+    {
+        private static readonly string[] AccessCodes = new[]
+        {
+            "Angel", "Dora", "Esther", "Alva", 
+        };
+
+        [HttpPost]
+        public IActionResult Post([FromQuery]String accessCode )
+        {
+            if (AccessCodes.Contains(accessCode))
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
