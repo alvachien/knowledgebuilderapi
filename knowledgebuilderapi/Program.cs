cef41b2796a5d14d697c64c495b53f44b2e2d0b9
Alva Chien <alvachien@live.com>
2021-10-23 23:22:07 +0800

    Add new codes

--- knowledgebuilderapi/Program.cs
+++ knowledgebuilderapi/Program.cs
@@ -6,19 +6,61 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace knowledgebuilderapi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+#else
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+#endif
+            .Enrich.FromLogContext()
+#if DEBUG
+            .WriteTo.Console()
+#else
+#if USE_ALIYUN
+            .WriteTo.File(
+                    @"C:\WebApps\Logs\KnowledgeBuilderAPI\log.txt",
+                    fileSizeLimitBytes: 1_000_000,
+                    rollOnFileSizeLimit: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(30))
+#endif
+#endif
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
