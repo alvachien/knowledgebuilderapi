e7723a7d69c1aa9742645c56e6e054199b4670b1
Alva Chien <alvachien@live.com>
2021-12-16 23:40:44 +0800

    Remove unnecessary AddCors

--- knowledgebuilderapi/Startup.cs
+++ knowledgebuilderapi/Startup.cs
@@ -49,7 +49,7 @@ namespace knowledgebuilderapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            //services.AddCors();
 
             if (Environment.IsDevelopment())
             {
